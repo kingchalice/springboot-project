b4f7c80cfc8d6497066e5be95cf9be4aab524e65
二十二桥枫别雨 <10739578+kingchalice@user.noreply.gitee.com>
2023-02-06 13:41:43 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/kingchalice/mywiki/controller/TestController.java
+++ src/main/java/com/kingchalice/mywiki/controller/TestController.java
@@ -1,26 +1,21 @@
 package com.kingchalice.mywiki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 //http://127.0.0.1:8880/hello
 @RestController //返回字符串
 //@Controller //返回页面
 public class TestController {
-    /**
-     * GET POST PUT DELETE
-     * /user?id=1
-     * /user/1
-     * @return
-     * */
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+    
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World! " + testHello;
     }
 
     @PostMapping ("/hello/post")
