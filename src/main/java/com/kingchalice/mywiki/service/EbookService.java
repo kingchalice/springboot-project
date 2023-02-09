bd1bb912f2141650c6b7e7dc51dd44ed47422521
二十二桥枫别雨 <10739578+kingchalice@user.noreply.gitee.com>
2023-02-09 10:10:40 +0800

    4-3 使用vue cli生成web项目

--- src/main/java/com/kingchalice/mywiki/service/EbookService.java
+++ src/main/java/com/kingchalice/mywiki/service/EbookService.java
@@ -6,7 +6,7 @@ import com.kingchalice.mywiki.domain.EbookExample;
 import com.kingchalice.mywiki.mapper.EbookMapper;
 import com.kingchalice.mywiki.req.EbookReq;
 import com.kingchalice.mywiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.kingchalice.mywiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -26,13 +26,17 @@ public class EbookService {
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook , ebookResp);
-            respList.add(ebookResp);
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook , ebookResp);
+//              //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//
+//        }
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
 
-        }
-
-        return respList;
+        return list;
     }
 }
