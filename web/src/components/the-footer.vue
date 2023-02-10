8a8d85b353a427d50332a0ed49a938edf8d94e0d
二十二桥枫别雨 <10739578+kingchalice@user.noreply.gitee.com>
2023-02-10 09:58:47 +0800

    4-7 制作the-footer组件,追上进度

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    kingchalice 首页布局开发
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+
+});
+</script>
